5e87c8ecebab00222c215c0a0cb8848edfe94d4b
wyonyango <wycliffealphus7@gmail.com>
2024-07-18 17:01:07 +0300

    feat:update recommended changes

---
+++ download/download.go
@@ -0,0 +1,31 @@
+package download
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// RenamingFile function accepts url and filename as arguments and saves downloaded file using the provided name
+func ToFile(url, filename string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("error downloading file: %v", err)
+	}
+
+	defer response.Body.Close()
+
+	output, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+
+	defer output.Close()
+
+	_, err = io.Copy(output, response.Body)
+	if err != nil {
+		return fmt.Errorf("error saving file: %v", err)
+	}
+	return nil
+}
