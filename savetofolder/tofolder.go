7f1a01145e6b50ed136d21791adff3430a57a64d
wyonyango <wycliffealphus7@gmail.com>
2024-07-24 15:41:47 +0300

    refactor:apply code re-use

--- savetofolder/tofolder.go
+++ savetofolder/tofolder.go
@@ -2,42 +2,28 @@ package savetofolder
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
+
+	"wget/download"
 )
 
-// ToFolder function takes url, outPutDir and filename as arguments and saves a download to a specified directory
+// ToFolder takes url, outputDir, and filename as arguments and saves a download to a specified directory
 func ToFolder(url, outputDir, filename string) error {
-	// Check if the outputDir file exists
-	err0 := os.MkdirAll(outputDir, os.ModePerm)
-	if err0 != nil {
-		return fmt.Errorf("error creating directory: %v", err0)
+	// Ensure the output directory exists
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error creating directory: %v", err)
 	}
 
+	// Generate the full output path
 	outputPath := filepath.Join(outputDir, filename)
 
-	response, err1 := http.Get(url)
-
-	if err1 != nil {
-		return fmt.Errorf("error fetching data:%v", err1)
+	// Call ToFile function to save the file
+	err = download.ToFile(url, outputPath)
+	if err != nil {
+		return fmt.Errorf("error saving file to folder: %v", err)
 	}
 
-	defer response.Body.Close()
-
-	outputFile, err2 := os.Create(outputPath)
-
-	if err2 != nil {
-		return fmt.Errorf("error creating directory: %v", err2)
-	}
-
-	defer outputFile.Close()
-
-	_, err3 := io.Copy(outputFile, response.Body)
-
-	if err3 != nil {
-		return fmt.Errorf("error saving data to the provided path: %v", err3)
-	}
 	return nil
 }
