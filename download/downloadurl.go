6d7532d00dd484f0d36b707cac9e6ff9dfb4e99c
shfana <wambitafana@gmail.com>
2024-07-29 12:16:12 +0300

    Refactor: separate validurl and downloadurl functions and tests

--- download/downloadurl.go
+++ download/downloadurl.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 )
@@ -46,34 +45,4 @@ func DownloadUrl(url string) error {
 		return fmt.Errorf("error saving file: %v", err)
 	}
 	return nil
-}
-
-// isValidURL checks if the given string is a valid URL
-func IsValidURL(urlStr string) bool {
-	parsedURL, err := url.Parse(urlStr)
-	fmt.Println(parsedURL)
-	if err != nil {
-		return false
-	}
-
-	// Check if the scheme is http or https
-	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
-		return false
-	}
-
-	// Check if the host is not empty
-	if parsedURL.Host == "" {
-		return false
-	}
-
-	// Ensure host is not empty and does not start with a dot or hyphen
-	if parsedURL.Host == "" || strings.HasPrefix(parsedURL.Host, ".") || strings.HasPrefix(parsedURL.Host, "-") {
-		return false
-	}
-
-	// Check if the host contains at least one dot
-	if !strings.Contains(parsedURL.Host, ".") {
-		return false
-	}
-	return true
 }
