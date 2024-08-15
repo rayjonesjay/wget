0e646fd9b6a5e2e94755daeef2a0b7fb3c0d260e
shfana <wambitafana@gmail.com>
2024-08-15 09:36:51 +0300

    Refactor: change function to check valid url

--- download/validUrl.go
+++ download/validUrl.go
@@ -1,37 +1,31 @@
 package download
 
 import (
-	"fmt"
 	"net/url"
 	"strings"
 )
 
 // isValidURL checks if the given string is a valid URL
 func IsValidURL(urlStr string) bool {
-	parsedURL, err := url.Parse(urlStr)
-	fmt.Println(parsedURL)
+	parsedURL, err := url.ParseRequestURI(urlStr)
 	if err != nil {
 		return false
 	}
 
-	// Check if the scheme is http or https
-	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+	// Ensure the URL is absolute and has a valid scheme (http or https)
+	if !parsedURL.IsAbs() || (parsedURL.Scheme != "http" && parsedURL.Scheme != "https") {
 		return false
 	}
 
-	// Check if the host is not empty
-	if parsedURL.Host == "" {
+	// Ensure the host is not empty, and doesn't start with a dot or hyphen
+	if strings.HasPrefix(parsedURL.Host, ".") || strings.HasPrefix(parsedURL.Host, "-") || parsedURL.Host == "" {
 		return false
 	}
 
-	// Ensure host is not empty and does not start with a dot or hyphen
-	if parsedURL.Host == "" || strings.HasPrefix(parsedURL.Host, ".") || strings.HasPrefix(parsedURL.Host, "-") {
-		return false
-	}
-
-	// Check if the host contains at least one dot
+	// Check that the host contains at least one dot (valid domain format)
 	if !strings.Contains(parsedURL.Host, ".") {
 		return false
 	}
+
 	return true
 }
