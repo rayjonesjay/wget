6b2b838a51f2c80b4eb8ab6c60bd740e490c6a78
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-18 05:04:14 +0300

    feat: read parsed arguments and parse them for evaluation

--- errors/errors.go
+++ errors/errors.go
@@ -1,2 +1,9 @@
 // errors package contains all error types, error messages and functions for handing errors
 package errors
+
+import "os"
+
+func WriteError(errorMessage string, statusCode int) {
+	os.Stdout.WriteString(errorMessage + "\n")
+	os.Exit(statusCode)
+}
