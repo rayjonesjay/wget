36e666c5cb5712344745f86f6449e6e5b6a7b19e
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-14 00:33:05 +0300

    feat: main function checks operating system,number of arguments and parses the arguments to be evaluated then triggers the download function

--- main.go
+++ main.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"wget/args"
+	"wget/help"
+
 	"wget/errorss"
 	"wget/syscheck"
-	"wget/types"
 )
 
 func main() {
 	if err := syscheck.CheckOperatingSystem(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
 	}
 	// download.DownloadUrl(errors.Link1)
 	arguments := os.Args[1:]
 	if len(arguments) == 0 {
 		// no arguments were passed, so return
-		errorss.WriteError(types.UsageMessage, 1)
+		errorss.WriteError(help.UsageMessage, 1)
 		return
 	}
 
 	// if arguments are passed.
-	args.EvalArgs(arguments)
+	arg := args.EvalArgs(arguments)
+
+	err := arg.Download()
+	if err != nil {
+		return
+	}
 }
