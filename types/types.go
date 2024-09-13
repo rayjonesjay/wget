40ec910527895abc9b04ae3285bc9252076d9192
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-14 01:17:31 +0300

    feat: arg type is defined here and its methods

--- types/types.go
+++ types/types.go
@@ -1,4 +1,4 @@
-// types contains user defined types and constants
+// Package types contains user defined types and constants
 package types
 
 import (
@@ -7,7 +7,46 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
+	"sync"
+)
+
+const (
+	// KB Size of Data in KiloBytes
+	KB = 1000 * 1
+
+	// KiB Size of Data in KibiBytes, same as 2^10
+	KiB = 1 << (10 * 1)
+
+	// MB Size of Data in MegaBytes
+	MB = 1000 * KB
+
+	// MiB Size of Data in MebiBytes, same as 2^20
+	MiB = 1 << 20
+
+	// GB Size of Data in GigaBytes
+	GB = 1000 * MB
+
+	// GiB Size of Data in GibiBytes, same as 2^30
+	GiB = 1 << 30
+
+	// TB Size of Data in TeraBytes
+	TB = 1000 * GB
+
+	// TiB Size of Data in TebiBytes, same as 2^40
+	TiB = 1 << 40
+)
+
+var (
+	// DefaultDownloadDirectory is the default location where files retrieved will reside.
+	DefaultDownloadDirectory = "$HOME/Downloads"
+
+	// DefaultHTTPPort is the default port used by http if not specified.
+	DefaultHTTPPort = "80"
+
+	// DefaultHTTPSPort is the default port used by https if not specified.
+	DefaultHTTPSPort = "443"
 )
 
 // Arg represents the commandline arguments passed through the command line by the user
@@ -32,50 +71,95 @@ type Arg struct {
 
 // Download handles normal downloads based on the provided URLs and other flags in the Arg struct
 func (a *Arg) Download() error {
-	successfulDownloads := []string{}
-	//failedDownloads := []string{}
+
+	var wg sync.WaitGroup
+
+	// if a download is successful, the link will be stored here.
+	successfulDownloads := make([]string, 0)
+
+	// channel to receive URLs that are successfully downloaded.
+	successChannel := make(chan string)
+
+	// channel to receive errors
+	errorChannel := make(chan error)
+
+	// start a go routine to handle successful downloads
+	go func() {
+		for url := range successChannel {
+			successfulDownloads = append(successfulDownloads, url)
+		}
+	}()
+
+	// start a go routine to handle errors
+	go func() {
+		for url := range errorChannel {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %s\n", url)
+		}
+	}()
+
 	for _, url := range a.Links {
-		// determine the output path and filename
-		outputFilePath := a.determineOutputPath(url)
+		wg.Add(1)
 
-		// open the output file for writing
-		outFile, err := os.Create(outputFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to create output file: %w", err)
-		}
-		defer outFile.Close()
+		go func(url string) {
 
-		// get the data
-		resp, err := http.Get(url)
-		if err != nil {
-			return fmt.Errorf("failed to download %s: %w", url, err)
-		}
-		defer resp.Body.Close()
+			defer wg.Done()
 
-		// copy the data to the outFile
-		_, err = io.Copy(outFile, resp.Body)
-		if err != nil {
-			return fmt.Errorf("failed to save URL %s to file %w", url, err)
-		}
-		successfulDownloads = append(successfulDownloads, url)
+			// determine the output path and filename
+			outputFilePath := a.determineOutputPath(url)
+
+			// open the output file for writing, create if it does not exist, truncate if it does exist.
+			outFile, err := os.OpenFile(outputFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+
+			if err != nil {
+				errorChannel <- fmt.Errorf("failed to open file for writing %w: %s", err, url)
+				return
+			}
+			defer outFile.Close()
+
+			// get the data
+			resp, err := http.Get(url)
+			fmt.Print("HTTP request sent, awaiting response...")
+			if err != nil {
+				errorChannel <- fmt.Errorf("failed to download %s: %w", url, err)
+				return
+			}
+
+			if resp.StatusCode == 200 {
+				a.LogDownloadInfo(resp)
+			} // else log errors here
+
+			defer resp.Body.Close()
+
+			// copy the data to the outFile
+			n, err := io.Copy(outFile, resp.Body)
+			if err != nil {
+				errorChannel <- fmt.Errorf("failed to save URL %s to file %w", url, err)
+				return
+			}
+			fmt.Printf("downloaded %d bytes from %s\n", n, url)
+
+			// successful download
+			successChannel <- url
+		}(url)
 	}
-	// TODO Print all the urls after successful download
-	if len(successfulDownloads) > 0 {
-		printUrls(successfulDownloads)
+
+	// wait for all downloading to complete
+	wg.Wait()
+	close(successChannel)
+	close(errorChannel)
+
+	//fmt.Println(">>>")
+	for _, successURL := range successfulDownloads {
+		successfulDownloads = append(successfulDownloads, successURL)
 	}
+	printUrls(successfulDownloads)
 	return nil
 }
 
-func printUrls(urls []string) {
-	res := ""
-	for i, url := range urls {
-		if i != len(urls)-1 {
-			res += url + " "
-		} else {
-			res += url
-		}
-	}
-	fmt.Printf("Download finished:	[%s]\n", res)
+func (a *Arg) LogDownloadInfo(response *http.Response) {
+	code := response.StatusCode
+	msg := fmt.Sprintf("\rHTTP request sent, awaiting response... %d OK", code)
+	fmt.Println(msg)
 }
 
 // determineOutputPath determines the full path for the output file
@@ -99,75 +183,39 @@ func (a *Arg) determineOutputPath(url string) string {
 	return outputFilePath
 }
 
-func (a *Arg) downloadFile(url, outputFilePath string) error {
-
-	// open the output file for writing
-	outputFile, err := os.Create(outputFilePath)
-	if err != nil {
-		return fmt.Errorf("failed to create output file: %w", err)
+func printUrls(urls []string) {
+	res := ""
+	for i, url := range urls {
+		if i != len(urls)-1 {
+			res += url + " "
+		} else {
+			res += url
+		}
 	}
-
+	fmt.Printf("Download finished:	[%s]\n", res)
 }
 
-const (
-	// Size of Data in KilobBytes
-	KB = 1000 * 1
+// IsEmpty function checks whether an iterable is empty, an iterable is a string,array or slice.
+// it returns true if the `data` which is expected to be an iterable, is empty else return false
+func (a *Arg) IsEmpty(data interface{}) bool {
 
-	// Size of Data in KibiBytes, same as 2^10
-	KiB = 1 << (10 * 1)
+	// get the value of the interface
+	val := reflect.ValueOf(data)
 
-	// Size of Data in MegaBytes
-	MB = 1000 * KB
+	// determine which kind of iterable object it is.
 
-	// Size of Data in MebiBytes, same as 2^20
-	MiB = 1 << 20
+	switch val.Kind() {
 
-	// Size of Data in GigaBytes
-	GB = 1000 * MB
+	// if it's a string, check if its empty
+	case reflect.String:
+		return strings.TrimSpace(val.String()) == ""
 
-	// Size of Data in GibiBytes, same as 2^30
-	GiB = 1 << 30
+	// if it's an array or a slice
+	case reflect.Array, reflect.Slice:
+		return val.Len() == 0
 
-	// Size of Data in TeraBytes
-	TB = 1000 * GB
-
-	// Size of Data in TebiBytes, same as 2^40
-	TiB = 1 << 40
-)
-
-var (
-	// Display usage message to user on how to use our program
-	UsageMessage = `wget: missing URL
-Usage: $ ./wget [OPTION] [URL]
-
-Try './wget --help' for more options.`
-
-	Manual = `wget 1.0.0, a non-interactive retriever.
-Usage: ./wget [OPTION] [URL]
-
-Supported options as of version 1.0.0:
-
-FLAG:				 USAGE:						EXPLANATION:
-
---help				 ******						print this manual
--B 	  		        -B URL					        allow download in the background after startup
--v 			        ******					        display the current version of wget and exit
--O   				-O=FILE	 URL					log messages to FILE
---mirror  			--mirror URL					mirror a website
--P			        -P=PATH  URL				        specify path to save downloaded resource
---rate-limit=NS									set speed limit, N is a number, S is either (k or M)
--i  				-i=FILE						allow download of resource by reading links stored in a file
-
-
-bug reports, questions, issues to https://github.com/rayjonesjay
-`
-	Version = "1.0.0"
-
-	DefaultDownloadDirectory = "$HOME/Downloads"
-
-	// default port used by http if not specified
-	DefaultHTTPPort = "80"
-
-	// default port used by https if not specified
-	DefaultHTTPSPort = "443"
-)
+	// for any other type return false, meaning it's not empty or object is not an iterable
+	default:
+		return false
+	}
+}
