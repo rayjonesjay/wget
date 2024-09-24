24def629ee27a03d23eb6251d23637831ef8a0f1
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-25 00:18:24 +0300

    Add: unit test for the relevant functions in eval_args.go

--- args/eval_args_test.go
+++ args/eval_args_test.go
@@ -39,12 +39,68 @@ func TestIsPathFlag(t *testing.T) {
 		{"test1", "-P=/home/Downloads", true, "/home/Downloads"},
 		{"test2", "-P=/home", true, "/home"},
 		{"test3", "-P=", false, ""},
+		{"test4", "-P=...", true, "..."},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got1, got2 := IsPathFlag(tt.input)
 			if got1 != tt.want1 || got2 != tt.want2 {
 				t.Errorf("IsPathFlag() = [%v %v], want [%v %v]", got1, got2, tt.want1, tt.want2)
+			}
+		})
+	}
+}
+
+// TestInputFile is a test function for InputFile function
+func TestInputFile(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want1 bool
+		want2 string
+	}{
+		{"test1", "-i=file.txt", true, "file.txt"},
+		{"test2", "-i=file", true, "file"},
+		{"test3", "-i", false, ""},
+		{"test4", "-i=.", false, ""},
+		{"test5", "-i=..", false, ""},
+		{"test6", "-i=...", true, "..."},
+		{"test7", "-i=/", false, ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got1, got2 := InputFile(tt.input)
+			if got1 != tt.want1 || got2 != tt.want2 {
+				t.Errorf("InputFile() = [%v %v] , want [%v %v]", got1, got2, tt.want1, tt.want2)
+			}
+		})
+	}
+}
+
+// TestIsOutputFlag is a test function for IsOutputFlag function
+func TestIsOutputFlag(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want1 bool
+		want2 string
+	}{
+		{"test1", "-O=file.txt", true, "file.txt"},
+		{"test2", "-O=file", true, "file"},
+		{"test3", "-O=", false, ""},
+		{"test4", "-o=file.txt", false, ""},
+		{"test5", "-O=/", false, ""},
+		{"test6", "-O=.", false, ""},
+		{"test7", "-O=..", false, ""},
+		{"test8", "-O=...", true, "..."},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got1, got2 := IsOutputFlag(tt.input)
+			if got1 != tt.want1 || got2 != tt.want2 {
+				t.Errorf("IsOutputFlag() = got [%v %v], want [%v %v]", got1, got2, tt.want1, tt.want2)
 			}
 		})
 	}
