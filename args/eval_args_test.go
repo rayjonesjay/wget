5557546a356f2da7ba1351475e9516080d9c29f3
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-24 20:50:21 +0300

    Add: new test function for IsHelpFlag

--- args/eval_args_test.go
+++ args/eval_args_test.go
@@ -4,6 +4,26 @@ import (
 	"testing"
 )
 
-func TestEvalArgs(t *testing.T) {
+// TestIsHelpFlag is a test file for the IsHelpFlag function
+func TestIsHelpFlag(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  bool
+	}{
+		{"test1", "--help", true},
+		{"test2", "-help", false},
+		{"test3", "-h", false},
+		{"test4", "help", false},
+		{"test5", "h", false},
+		{"test6", "", false},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsHelpFlag(tt.input); got != tt.want {
+				t.Errorf("IsHelpFlag() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
