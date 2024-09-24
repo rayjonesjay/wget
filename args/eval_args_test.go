2e2bcb8cf82e7a11a6b3fa78f37e99dc1fc41748
rayjonesjay <rayjaymuiruri@gmail.com>
2024-09-24 21:20:29 +0300

    Add: new test function for IsPathFlag

--- args/eval_args_test.go
+++ args/eval_args_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-// TestIsHelpFlag is a test file for the IsHelpFlag function
+// TestIsHelpFlag is a test function for the IsHelpFlag function
 func TestIsHelpFlag(t *testing.T) {
 	tests := []struct {
 		name  string
@@ -23,6 +23,28 @@ func TestIsHelpFlag(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsHelpFlag(tt.input); got != tt.want {
 				t.Errorf("IsHelpFlag() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// TestIsPathFlag is a test function for IsPathFlag function
+func TestIsPathFlag(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want1 bool
+		want2 string
+	}{
+		{"test1", "-P=/home/Downloads", true, "/home/Downloads"},
+		{"test2", "-P=/home", true, "/home"},
+		{"test3", "-P=", false, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got1, got2 := IsPathFlag(tt.input)
+			if got1 != tt.want1 || got2 != tt.want2 {
+				t.Errorf("IsPathFlag() = [%v %v], want [%v %v]", got1, got2, tt.want1, tt.want2)
 			}
 		})
 	}
