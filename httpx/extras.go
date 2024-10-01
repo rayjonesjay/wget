354da4d66eaeb7dc1d65609523cc62c2277398d2
najwang <anyonanicholas86@gmail.com>
2024-10-01 21:18:51 +0300

    feat: http utilities to extract mime-type and content-length from http headers

---
+++ httpx/extras.go
@@ -0,0 +1,41 @@
+// Package httpx creates convenience utility wrappers around the standard's http package
+package httpx
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// ExtractMimeType extracts the MIME type of the response body, from HTTP response headers.
+func ExtractMimeType(headers http.Header) string {
+	contentType := headers.Get("Content-Type")
+	if contentType == "" {
+		return ""
+	}
+
+	// Content-Type can have parameters (e.g., "text/html; charset=utf-8")
+	// We only want the main MIME type part.
+	parts := strings.Split(contentType, ";")
+	// since our sep argument to strings.Split is not empty, strings.Split must
+	// return an array of at least length 1, thus we can safely access index 0
+	mimeType := strings.TrimSpace(parts[0])
+
+	return mimeType
+}
+
+// ExtractContentLength extracts the Content-Length from HTTP response headers as an int.
+func ExtractContentLength(headers http.Header) int64 {
+	contentLengthStr := headers.Get("Content-Length")
+	if contentLengthStr == "" {
+		return -1
+	}
+
+	contentLengthStr = strings.TrimSpace(contentLengthStr)
+	contentLength, err := strconv.ParseInt(contentLengthStr, 10, 64)
+	if err != nil {
+		return -1
+	}
+
+	return contentLength
+}
