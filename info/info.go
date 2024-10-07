e9ddf10994ef40355ae64318b1f6055edb9adf75
najwang <anyonanicholas86@gmail.com>
2024-10-07 18:01:22 +0300

    feat: program information

---
+++ info/info.go
@@ -0,0 +1,10 @@
+// Package info contains various information about the program, made available to the program at runtime
+package info
+
+var (
+	// Org the name of the organization owning the program
+	Org     = "org.zone01.wget"
+	Name    = "wget"
+	Version = "1.1.0"
+	Go      = "1.22.4"
+)
