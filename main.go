a46a55b69c1e1373071fedef6f0cf8cf58f8a347
najwang <anyonanicholas86@gmail.com>
2024-10-07 18:02:18 +0300

    feat: configure program logging with the temp package

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"runtime"
 	"wget/fileio"
+	"wget/temp"
 
 	"wget/args"
 	"wget/downloader"
@@ -30,8 +32,8 @@ func main() {
 		return
 	}
 
-	// Configure logging to application logger file /tmp/com.zone01.wget/logger.log
-	logger, err := os.OpenFile("/tmp/com.zone01.wget/logger.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	// configure file logging to temporary application logger file
+	logger, err := os.OpenFile(path.Join(temp.Dir(), "logger.log"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Printf("failed to setup file logging: logging to stderr instead: %v\n", err)
 	}
