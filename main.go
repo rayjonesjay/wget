0647333a771230521b066abc87d4f443469f731e
najwang <anyonanicholas86@gmail.com>
2024-10-07 10:48:27 +0300

    feat: configure global file-based logging

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"runtime"
+	"wget/fileio"
 
 	"wget/args"
 	"wget/downloader"
@@ -28,6 +30,15 @@ func main() {
 		return
 	}
 
+	// Configure logging to application logger file /tmp/com.zone01.wget/logger.log
+	logger, err := os.OpenFile("/tmp/com.zone01.wget/logger.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Printf("failed to setup file logging: logging to stderr instead: %v\n", err)
+	}
+	log.SetOutput(logger)
+	defer fileio.Close(logger)
+
+	// check command-line args and download the defined files
 	ctx := args.DownloadContext(arguments)
 	downloader.Get(ctx)
 }
