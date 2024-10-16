28910744c85e494e7c71e1a99f4e8a3c389a7b9f
rayjonesjay <rayjaymuiruri@gmail.com>
2024-10-16 12:50:41 +0300

    removed test that checked if flag is --help

--- args/eval_args_test.go
+++ args/eval_args_test.go
@@ -6,32 +6,6 @@ import (
 	"wget/ctx"
 )
 
-// TestIsHelpFlag is a test function for the IsHelpFlag function
-func TestIsHelpFlag(t *testing.T) {
-	tests := []struct {
-		name  string
-		input string
-		want  bool
-	}{
-		{"test1", "--help", true},
-		{"test2", "-help", false},
-		{"test3", "-h", false},
-		{"test4", "help", false},
-		{"test5", "h", false},
-		{"test6", "", false},
-	}
-
-	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				if got := IsHelpFlag(tt.input); got != tt.want {
-					t.Errorf("IsHelpFlag() = %v, want %v", got, tt.want)
-				}
-			},
-		)
-	}
-}
-
 // TestIsPathFlag is a test function for IsPathFlag function
 func TestIsPathFlag(t *testing.T) {
 	tests := []struct {
