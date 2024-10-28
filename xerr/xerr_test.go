a8c91cc6b636af780ba2ba936752da02d9820e0f
najwang <anyonanicholas86@gmail.com>
2024-10-28 22:46:10 +0300

    test: function delegation to improve tests

--- xerr/xerr_test.go
+++ xerr/xerr_test.go
@@ -1,12 +1,10 @@
 // Write error message to standard error successfully
-package xerr_test
+package xerr
 
 import (
 	"bytes"
 	"os"
 	"testing"
-
-	"wget/xerr"
 )
 
 func TestWriteErrorMessageToStderr(t *testing.T) {
@@ -15,7 +13,7 @@ func TestWriteErrorMessageToStderr(t *testing.T) {
 	os.Stderr = w
 
 	errorMessage := "Test error message"
-	xerr.WriteError(errorMessage, 0, false)
+	WriteError(errorMessage, 0, false)
 
 	w.Close()
 	var buf bytes.Buffer
@@ -35,7 +33,7 @@ func TestHandleNonStringErrorMessage(t *testing.T) {
 	os.Stderr = w
 
 	errorMessage := 12345
-	xerr.WriteError(errorMessage, 0, false)
+	WriteError(errorMessage, 0, false)
 
 	w.Close()
 	var buf bytes.Buffer
@@ -45,5 +43,79 @@ func TestHandleNonStringErrorMessage(t *testing.T) {
 	expectedOutput := "12345\n"
 	if buf.String() != expectedOutput {
 		t.Errorf("Expected %v, got %v", expectedOutput, buf.String())
+	}
+}
+
+func Test_writeError(t *testing.T) {
+	lastCode := -1
+	mockExit := func(code int) {
+		lastCode = code
+	}
+
+	type args struct {
+		errorMessage interface{}
+		statusCode   int
+		shouldExit   bool
+		exit         func(int)
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Base",
+			args: args{
+				errorMessage: "Test message",
+				statusCode:   0,
+				shouldExit:   false,
+				exit:         mockExit,
+			},
+		},
+
+		{
+			name: "Error: exit code 1",
+			args: args{
+				errorMessage: "Test message",
+				statusCode:   1,
+				shouldExit:   true,
+				exit:         mockExit,
+			},
+		},
+
+		{
+			name: "Error: exit code 0",
+			args: args{
+				errorMessage: "Test message",
+				statusCode:   0,
+				shouldExit:   true,
+				exit:         mockExit,
+			},
+		},
+
+		{
+			name: "Redundant: exit code 1",
+			args: args{
+				errorMessage: "Test message",
+				statusCode:   1,
+				shouldExit:   false,
+				exit:         mockExit,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			writeError(tt.args.errorMessage, tt.args.statusCode, tt.args.shouldExit, tt.args.exit)
+			if tt.args.shouldExit {
+				if lastCode != tt.args.statusCode {
+					t.Errorf("Expected %v, got %v", tt.args.statusCode, lastCode)
+				}
+			} else {
+				if lastCode != -1 {
+					t.Errorf("Expected %v, got %v", -1, lastCode)
+				}
+			}
+
+			lastCode = -1
+		})
 	}
 }
